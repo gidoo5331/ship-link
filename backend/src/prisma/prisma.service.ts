4a374bd4e0aeecd5fd0e30d2c5c62d24d58e4f01
gidoo5331 <gideonbempong@gmail.com>
2026-06-09 14:24:57 +0000

    prisma module

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(config: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: config.get('DATABASE_URL'),
+    });
+    super({ adapter });
+  }
+}
